dd4db94eae2957f199b924bd236e124e1135e9cd
shaomin <sam880819@gmail.com>
2019-12-09 18:28:16 +0800

    good

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-$('')
+$(document).ready(() => {
+    for (let index = 0; index < 5; index++) {
+        var d1_switch_obj = `<label class="switch">
+        <input type="checkbox" class="chk" id="device_1_${index}">
+        <span class="slider round"></span>
+        </label>`;
+        var d2_switch_obj = `<label class="switch">
+        <input type="checkbox" class="chk" id="device_2_${index}">
+        <span class="slider round"></span>
+        </label>`;
+        $('.device_1').append(d1_switch_obj);
+        $('.device_2').append(d2_switch_obj);
+    }
+
+});
