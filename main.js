d9b2f5e6118cb376c6abc5b2d046636a05fb7be2
shaomin <sam880819@gmail.com>
2019-12-24 17:44:04 +0800

    v1.0

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 $(document).ready(() => {
-    for (let index = 0; index < 5; index++) {
-        var d1_switch_obj = `<label class="switch">
-        <input type="checkbox" class="chk" id="device_1_${index}">
-        <span class="slider round"></span>
-        </label>`;
-        var d2_switch_obj = `<label class="switch">
-        <input type="checkbox" class="chk" id="device_2_${index}">
-        <span class="slider round"></span>
-        </label>`;
-        $('.device_1').append(d1_switch_obj);
-        $('.device_2').append(d2_switch_obj);
-    }
 
+    $('#light').on("change",()=>{
+        
+        if ($('#light').is(":checked")) {
+            
+            $.get('http://192.168.0.18/on');
+        }else{
+            
+            $.get('http://192.168.0.18/off');
+        }
+    });
 });
