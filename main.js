42a59516f14d3ddc5adcc7fda9636264885063f1
shaomin <sam880819@gmail.com>
2019-12-26 21:06:10 +0800

    123

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 $(document).ready(() => {
     var led = firebase.database().ref('blueLight');
-    $('#light').on("change", () => {
-        if ($('#light').is(":checked")) {
+    $('.light').on('click', () => {
+        if ($('.light').is(":checked")) {
             led.set(1);
         } else {
             led.set(0);
         }
     });
     led.on('value', (r) => {
+        $('.light').click();
         if (r.val() == 1) {
             console.log('目前打開');
-            $('#light').attr("checked","true");
+            $('#light_status').html('Light_On');
+
         } else {
             console.log('目前關閉');
-            $('#light').attr("checked",false);
+            $('#light_status').html('Light_Off');
         }
     });
+
 });
