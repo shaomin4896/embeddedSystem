81a8e2db5ec9e8cf1081f7a2ba8feee27d60c731
shaomin <sam880819@gmail.com>
2019-12-26 19:00:52 +0800

    ohyesyesyes

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 $(document).ready(() => {
-
+    var led = firebase.database().ref('blueLight');
     $('#light').on("change", () => {
-        var led = firebase.database().ref('blueLight');
         if ($('#light').is(":checked")) {
             led.set(1);
         } else {
             led.set(0);
+        }
+    });
+    led.on('value', (r) => {
+        if (r.val() == 1) {
+            console.log('目前打開');
+            $('#light').attr("checked","true");
+        } else {
+            console.log('目前關閉');
+            $('#light').attr("checked",false);
         }
     });
 });
