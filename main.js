4a6a411bd5a6ca6ddd59aa14bdd77acac3caedc3
shaomin <sam880819@gmail.com>
2019-12-27 13:29:07 +0800

    add_servo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     var led = firebase.database().ref('blueLight');
+    var servo = firebase.database().ref('servo');
     $('.light').on('click', () => {
         if ($('.light').is(":checked")) {
             led.set(1);
@@ -7,15 +8,29 @@ $(document).ready(() => {
             led.set(0);
         }
     });
+    $('.servo').on('click',()=>{
+        if ($('.servo').is(":checked")) {
+            servo.set(1);
+        } else {
+            servo.set(0);
+        }
+    });
     led.on('value', (r) => {
         if (r.val() == 1) {
-            console.log('目前打開');
             $('.switch_blueLight').addClass('is-checked');
-            $('#light_status').html('Light_On');
+            $('#light_status').html('BlueLight_On');
         } else {
-            console.log('目前關閉');
             $('.switch_blueLight').removeClass('is-checked');
-            $('#light_status').html('Light_Off');
+            $('#light_status').html('BlueLight_Off');
+        }
+    });
+    servo.on('value', (r) => {
+        if (r.val() == 1) {
+            $('.switch_servo').addClass('is-checked');
+            $('#servo_status').html('SwitchServo_On');
+        } else {
+            $('.switch_servo').removeClass('is-checked');
+            $('#servo_status').html('SwitchServo_Off');
         }
     });
 });
