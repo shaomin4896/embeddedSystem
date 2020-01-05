16a4b75ab169396bbd22496fe51aeaa95195da2a
shaomin <sam880819@gmail.com>
2020-01-05 23:54:16 +0800

    lastest

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
     var led = firebase.database().ref('blueLight');
     var dht = firebase.database().ref('dht');
+    var door = firebase.database().ref('door');
+
     $('.light').on('click', () => {
         if ($('.light').is(":checked")) {
             led.set(1);
@@ -8,7 +10,34 @@ $(document).ready(() => {
             led.set(0);
         }
     });
-    dht.on('value',(r)=>{
+    $('.door').on('click', () => {
+        $('.pwd').show();
+    });
+    $('.submit').on('click', () => {
+        pwd = $('.pwdtext').val();
+        var pwdref = firebase.database().ref('pwd');
+        pwdref.once('value', (r) => {
+            if (pwd == r.val()) {
+                door.set(1);
+            } else {
+                door.set(0);
+            }
+        });
+    });
+    $('.close_submit').on('click', () => {
+        pwd = $('.pwdtext').val();
+        var pwdref = firebase.database().ref('pwd');
+        pwdref.once('value', (r) => {
+            if (pwd == r.val()) {
+                door.set(0);
+            }
+        });
+    });
+    $('.reset').on('click', () => {
+        $('.pwd').hide();
+        $('.re').show();
+    });
+    dht.on('value', (r) => {
         dhtJson = r.val();
         $('.tem').html(`${dhtJson.dht_tem}°C`);
         $('.hum').html(`${dhtJson.dht_hum}.00%`);
@@ -22,6 +51,4 @@ $(document).ready(() => {
             $('#light_status').html('BlueLight_Off');
         }
     });
-
-
 });
